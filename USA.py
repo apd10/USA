98b565449ab82091d83eb9537f946cd0cd7170cc
Aditya Desai <apd10@rice.edu>
2024-10-05 18:52:07 -0500

    learning to hash + train _usa changes

--- USA.py
+++ USA.py
@@ -3,6 +3,8 @@ from torch import nn
 import math
 import numpy as np
 
+
+
 class USA(nn.Module):
     def __init__(self, num_heads, head_dim, usa_params = {'L': 1, 'R': 3, 'int_dim': 128, 'aug_k' : 0}, annealing_paramters = {'T': 10, 't': 0}, use_lsh=False):
         super(USA, self).__init__()
@@ -24,11 +26,22 @@ class USA(nn.Module):
 
         # TODO(what is the best way to initialize?)
         # TODO(move to class variables?)
-        self.uplift_K = nn.Linear(head_dim + self.aug_k, self.int_dim)
-        self.uplift_Q = nn.Linear(head_dim, self.int_dim)
+        self.FINAL_DIM = 32
+        self.uplift_K = nn.ModuleList([nn.Sequential(nn.Linear(head_dim + self.aug_k, self.int_dim), 
+                                      nn.SiLU(),
+                                      nn.Linear(self.int_dim, self.int_dim),
+                                      nn.SiLU(),
+                                      nn.Linear(self.int_dim, self.FINAL_DIM)
+                                    ) for i in range(self.num_heads)])
+        self.uplift_Q = nn.ModuleList([nn.Sequential(nn.Linear(head_dim, self.int_dim),
+                                      nn.SiLU(),
+                                      nn.Linear(self.int_dim, self.int_dim),
+                                      nn.SiLU(),
+                                      nn.Linear(self.int_dim, self.FINAL_DIM)
+                                      ) for i in range(self.num_heads)])
 
         self.W = nn.Parameter(torch.randn(self.num_heads, self.int_dim, self.L * self.R), requires_grad=True)
-
+        
 
     def aug_K(self, K, num):
         if num <= 0:
@@ -41,6 +54,36 @@ class USA(nn.Module):
         
 
     def forward(self, K, Q, hard=False):
+        b,a,sk,d = K.shape
+        _,_,sq,d = Q.shape
+
+        Klifted = torch.zeros((b,a,sk,self.FINAL_DIM), device=K.device)
+        Qlifted = torch.zeros((b,a,sq,self.FINAL_DIM), device=Q.device)
+        K1 = self.aug_K(K, self.aug_k)
+        for i in range(self.num_heads):
+            Klifted[:,i,:,:] = self.uplift_K[i](K1[:,i,:,:])
+            Qlifted[:,i,:,:] = self.uplift_Q[i](Q[:,i,:,:])
+
+        # get binary hash code
+        if hard:
+            Klifted = torch.sign(Klifted)
+            Qlifted = torch.sign(Qlifted)
+        else:
+            Klifted = nn.functional.tanh(Klifted)
+            Qlifted = nn.functional.tanh(Qlifted)
+        
+        span =  torch.einsum("basd,batd->bast", Qlifted, Klifted)
+
+        if hard:
+            span = (span - self.FINAL_DIM * self.t  > 0).float()
+        else:
+            span = torch.sigmoid(span - self.FINAL_DIM * self.t)
+
+        return span
+
+
+    def _forward(self, K, Q, hard=False):
+
         # shapes
         b,a,sk,d = K.shape
         _,_,sq,d = Q.shape
@@ -48,13 +91,17 @@ class USA(nn.Module):
         # uplift K and Q. TODO(try SIMPLE-LSH vs. learned linear projection)
         # common across attention heads
         if not self.lsh:
-            Klifted = self.uplift_K(self.aug_K(K, self.aug_k)) # b,a,sk,int_dim
-            Qlifted = self.uplift_Q(Q)
+            Klifted = torch.zeros((b,a,sk,self.int_dim), device=K.device)
+            Qlifted = torch.zeros((b,a,sq,self.int_dim), device=Q.device)
+            K1 = self.aug_K(K, self.aug_k)
+            for i in range(self.num_heads):
+                Klifted[:,i,:,:] = self.uplift_K[i](K1[:,i,:,:])
+                Qlifted[:,i,:,:] = self.uplift_Q[i](Q[:,i,:,:])
         else:
             M = torch.max(torch.norm(K, dim=-1, keepdim=True), dim=2, keepdim=True).values # b,a,1,1
             Klifted = K / M
             Klifted = torch.cat([Klifted, torch.sqrt(1 - torch.norm(Klifted, dim=-1, keepdim=True))], dim=-1)
-            Qlifted =  nn.functional.pad(Q,  (0,1), "constant", 0) 
+            Qlifted =  nn.functional.pad(Q,  (0,1), "constant", 0)
 
         # soft signed random projections
         if hard or self.lsh:
@@ -68,6 +115,7 @@ class USA(nn.Module):
         Kc = Kc.view(b, a, sk, self.L, self.R)
         Qc = Qc.view(b, a, sq, self.L, self.R)
         partition_count = torch.einsum("baslr,batlr->balts", Kc, Qc) / self.R
+        
         ## shifted silu since we need all partition to match eventually
         if hard or self.lsh:
             partition_count = ((partition_count - 1.0 + 1e-3) > 0).float()
--- train_usa.py
+++ train_usa.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from datasets import load_dataset
 import argparse
 from tqdm import tqdm
@@ -8,8 +9,16 @@ from transformers.cache_utils import DynamicCache
 import math
 import gc 
 import numpy as np
-
+DEBUG_SAMPLES = 10
 from USA import USA
+
+
+def loss_function(yhat ,ytarget, beta):
+    weight = ytarget * (beta - 1) + torch.ones_like(ytarget)
+    loss = nn.functional.binary_cross_entropy(yhat.reshape(-1), ytarget.reshape(-1), weight = weight.reshape(-1))
+    return loss
+
+
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     """
     This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
@@ -27,6 +36,9 @@ def load_data(name):
         dataset = load_dataset("FiscalNote/billsum")
     elif name == "qmsum":
         dataset = load_dataset("ioeddk/qmsum")
+    elif name.startswith("lb-"):
+        dname = name.split('-')[1]
+        dataset = load_dataset("THUDM/LongBench", dname)
     else:
         raise NotImplementedError
     
@@ -52,6 +64,14 @@ def prompt_builder(model_name, data_name):
     elif model_name == "llama3-inst" and data_name == "qmsum":
 
         def builder (instance):
+            messages = [{"role": "user", "content": '{}'.format(instance['text'].split("Answer:")[0])},
+            {"role" : "assistant", "content": '{}'.format(instance['answer'])}]
+            return messages
+        return builder
+    elif model_name == "llama3-inst" and data_name.startswith("lb-"):
+        def builder (instance):
+            import pdb
+            pdb.set_trace()
             messages = [{"role": "user", "content": '{}'.format(instance['text'].split("Answer:")[0])},
             {"role" : "assistant", "content": '{}'.format(instance['answer'])}]
             return messages
@@ -83,14 +103,23 @@ def compute_target_mask(attn_weights, mode):
         else:
             raise NotImplementedError
 
+def get_kq_mask(output):
+    B,A,S1,S2 = output.shape
+    mask  = torch.ones_like(output)
+    # do not consider first 64 tokens
+    mask[:,:,:,:64] = 0.
+    # do not consider last 64 tokens and causal mask
+    mask = mask * (torch.tril(mask, diagonal=-64) !=0).float()
+    return mask
 
-def evaluate_coverage(model, usas, test_data_loader, num_samples=10):
-    coverages = torch.zeros(len(usas), usas[0].num_heads, device="cuda:0")
-    sparsities = torch.zeros(len(usas), usas[0].num_heads, device="cuda:0")
+
+def evaluate_coverage(model, usas, test_data_loader, target_mode, num_queries, num_samples=10):
+    recalls = torch.zeros(len(usas), device="cuda:0")
+    precisions = torch.zeros(len(usas), device="cuda:0")
     num = 0
     for (batch_idx, batch) in tqdm(enumerate(test_data_loader), total=min(num_samples, test_data_loader.__len__())):
-        if batch_idx > num_samples:
-            break
+        if batch_idx == num_samples:
+             break
         with torch.no_grad():        
             output_dict = model(input_ids = batch['input_ids'].to("cuda:1"),
                     attention_mask = batch['attention_mask'].to("cuda:1"),
@@ -105,73 +134,108 @@ def evaluate_coverage(model, usas, test_data_loader, num_samples=10):
             usa = usas[i].to("cuda:0")
             K = output_dict['past_key_values'].key_cache[i].detach().to("cuda:0")
             Q = output_dict['past_key_values'].query_cache[i].detach().to("cuda:0")
-            Q = Q[:,:,Q.shape[2]//2:,:]
             K = repeat_kv(K, Q.shape[1] // K.shape[1])
             sspan = usa(K, Q, hard=True)
-            target = torch.nn.functional.softmax(torch.matmul(Q, K.transpose(2,3)) / math.sqrt(K.shape[-1]), dim=-1, dtype=torch.float32).to(Q.dtype)
-            coverage = torch.sum(target * sspan, dim=-1).mean(dim=-1).mean(dim=0)
-            coverages[i] += coverage
+            kq_mask = get_kq_mask(sspan)
+            raw_attention = torch.matmul(Q, K.transpose(2,3)) / math.sqrt(K.shape[-1])
+            raw_attention = raw_attention + (kq_mask - 1.0)*1e35
+            target = torch.nn.functional.softmax(raw_attention, dim=-1, dtype=torch.float32).to(Q.dtype)
+            target_sspan = compute_target_mask(target, target_mode) * kq_mask
+
+            target = target * kq_mask
+            sspan = sspan * kq_mask
+            target_sspan = target_sspan[:,:,-num_queries:,:]
+            sspan = sspan[:,:,-num_queries:,:]
+            kq_mask = kq_mask[:,:,-num_queries:,:]
+            T = target_sspan.reshape(-1)[kq_mask.reshape(-1) >  1e-6]
+            A = sspan.reshape(-1)[kq_mask.reshape(-1) > 1e-6]
+
+            recall = torch.sum(T[A > 1e-6]) / torch.sum(T)
+            precision = torch.sum(A[T > 1e-6]) / torch.sum(A)
+            recalls[i] += recall
             
-            sparsity = sspan.mean(dim=-1).mean(dim=-1).mean(dim=0)
-            sparsities[i] += sparsity
+            precisions[i] += precision
+            # for j in range(32):
+            #     print("layer,att: ",i,j, torch.mean((sspan[0][j][0] == target_sspan[0][j][0]).float()))
+            # print("precision", precision, "recall", recall)
+            # import pdb
+            # pdb.set_trace()
 
         num+=1
 
-    coverages = coverages / num
-    sparsities = sparsities / num
-    print("--- coverage ---")
-    print(coverages)
-    print("--- sparsities ---")
-    print(sparsities)
-    return {'coverage' : torch.mean(coverages).item(),
-            'sparsity' : torch.mean(sparsities).item()}
+    recalls = recalls / num
+    precisions = precisions / num
+    print("--- recall ---")
+    print(recalls)
+    print("--- precisions ---")
+    print(precisions)
+    return {'recall' : torch.mean(recalls).item(),
+            'precision' : torch.mean(precisions).item()}
 
 
 
-def train_step(usa, K, Q, target_mode, span_criterion):
+
+def train_step(usa, K, Q, target_mode, span_criterion, num_queries, recall_weight):
     # GQA
     K = repeat_kv(K, Q.shape[1] // K.shape[1])
-    Q = Q[:,:,Q.shape[2]//2:,:] # only use last half of queries
     output = usa(K, Q)
-    attention_scores = torch.nn.functional.softmax(torch.matmul(Q, K.transpose(2,3)) / math.sqrt(K.shape[-1]), dim=-1, dtype=torch.float32).to(Q.dtype)
-    target = compute_target_mask(attention_scores, target_mode)
-    span_loss = span_criterion(torch.nn.functional.relu(output), target)
+    train_mask = get_kq_mask(output)
+
+    raw_attention_scores = torch.matmul(Q, K.transpose(2,3)) / math.sqrt(K.shape[-1])
+    raw_attention_scores = raw_attention_scores + ( train_mask - 1.0 ) * 1e35
+    attention_scores = torch.nn.functional.softmax(raw_attention_scores, dim=-1, dtype=torch.float32).to(Q.dtype)
+
+    attention_scores = attention_scores * train_mask
+    output = output * train_mask
+    target = compute_target_mask(attention_scores, target_mode) * train_mask
+    # keep only half queries
+
+    span_loss = span_criterion(torch.nn.functional.relu(output)[:,:,-num_queries:,:], target[:,:,-num_queries:,:], beta=recall_weight)
     return span_loss
 
-def train_usa(model, usas, train_data_loader, test_data_loader, target_mode, optimizer, span_criterion, max_train_itr):
+def train_usa(model, usas, train_data_loader, test_data_loader, target_mode, optimizer, span_criterion, max_train_itr, num_queries, accumulate_grad, recall_weight,args):
     itr = 0
-    for (batch_idx, batch) in tqdm(enumerate(train_data_loader), total=train_data_loader.__len__()):
-            itr += 1
-            if max_train_itr > 0 and itr > max_train_itr:
-                break
-            if batch_idx % 10 == 0:
-                result = evaluate_coverage(model, usas, test_data_loader, num_samples=10)
-                print('{}/{} coverage:{:.6f} sparsity:{:.6f}'.format(batch_idx, train_data_loader.__len__(), result['coverage'], result['sparsity']))
+    for epoch in range(1000):
+        for (batch_idx, batch) in tqdm(enumerate(train_data_loader), total=train_data_loader.__len__()):
 
-                torch.save(usas.cpu().state_dict(), "./artifacts/usa.pt")
+                # if batch_idx == DEBUG_SAMPLES:
+                #     break
+                if epoch == 0 and batch_idx < args.skip_examples:
+                    continue
+                if max_train_itr > 0 and itr > max_train_itr:
+                    break
+                if itr % 64 == 0:
+                    result = evaluate_coverage(model, usas, train_data_loader, target_mode, num_queries=num_queries, num_samples=10)
+                    print('TRAIN {}/{} recall:{:.6f} precision:{:.6f}'.format(batch_idx, train_data_loader.__len__(), result['recall'], result['precision']))
+                    result = evaluate_coverage(model, usas, test_data_loader, target_mode, num_queries=num_queries, num_samples=100)
+                    print('TEST {}/{} recall:{:.6f} precision:{:.6f}'.format(batch_idx, train_data_loader.__len__(), result['recall'], result['precision']))
 
-            with torch.no_grad():        
-                output_dict = model(input_ids = batch['input_ids'].to("cuda:1"),
-                        attention_mask = batch['attention_mask'].to("cuda:1"),
-                        past_key_values=DynamicCache(),
-                        use_cache=True,
-                        output_attentions=True,
-                        return_dict=True)
+                    torch.save(usas.cpu().state_dict(), "./artifacts/usa.pt")
 
-            total_loss = 0
-            losses = []
-            for i in range(len(usas)):
-                usa = usas[i].to("cuda:0")
-                K = output_dict['past_key_values'].key_cache[i].detach().to("cuda:0")
-                Q = output_dict['past_key_values'].query_cache[i].detach().to("cuda:0")
-                loss = train_step(usa, K, Q, target_mode, span_criterion)
-                total_loss += loss
-                losses.append(loss.item())
-            optimizer.zero_grad()
-            total_loss.backward()
-            optimizer.step()
-            print(np.mean(losses), losses, flush=True)
-            
+                with torch.no_grad():        
+                    output_dict = model(input_ids = batch['input_ids'].to("cuda:1"),
+                            attention_mask = batch['attention_mask'].to("cuda:1"),
+                            past_key_values=DynamicCache(),
+                            use_cache=True,
+                            output_attentions=True,
+                            return_dict=True)
+
+                total_loss = 0
+                losses = []
+                for i in range(len(usas)):
+                    usa = usas[i].to("cuda:0")
+                    K = output_dict['past_key_values'].key_cache[i].detach().to("cuda:0")
+                    Q = output_dict['past_key_values'].query_cache[i].detach().to("cuda:0")
+                    loss = train_step(usa, K, Q, target_mode, span_criterion, num_queries, recall_weight)
+                    total_loss += loss
+                    losses.append(loss.item())
+
+                total_loss.backward()
+                if itr % accumulate_grad == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+                print(np.mean(losses), losses, flush=True)
+                itr += 1
 
 
 def data_pipeline(train_data, tokenizer, builder, max_len):
@@ -193,7 +257,7 @@ def main():
     parser = argparse.ArgumentParser(description=" Train Script for USA")
 
     # Add arguments
-    parser.add_argument("--dataset", type=str, help="dataset", choices=["billsum", "qmsum"])
+    parser.add_argument("--dataset", type=str, help="dataset")
     parser.add_argument("--model", type=str, help="model", choices=["llama3-inst"])
     parser.add_argument("--train_epochs", type=int, help="train args: epochs", default=1)
     parser.add_argument("--batch", type=int, help="train args: batchsize", default=1)
@@ -209,6 +273,11 @@ def main():
     parser.add_argument("--usa-int-dim", type=int, help="usa int dim", default=256)
     parser.add_argument("--usa-aug-k", type=int, help="usa aug k", default=0)
     parser.add_argument("--usa-target-mode", type=str, help="usa aug k", default="top-10")
+    parser.add_argument("--usa-t", type=float, help="usa t", default=0.5)
+    parser.add_argument("--accumulate-grad", type=int, help="usa acc grad", default=8)
+    parser.add_argument("--num-queries", type=int, help="num queries for training and testing", default=8)
+    parser.add_argument("--recall-weight", type=float, help="num queries for training and testing", default=10.0)
+    parser.add_argument("--skip-examples", type=int, help="skip examples", default=0)
 
 
     args = parser.parse_args()
@@ -254,17 +323,18 @@ def main():
         usas.append( USA( model.model.layers[0].self_attn.num_heads, 
                         model.model.layers[i].self_attn.head_dim,
                         usa_params = {'L': args.usa_L, 'R': args.usa_R, 'int_dim': args.usa_int_dim, 'aug_k' : 0},
-                        annealing_paramters = {'T': 10, 't': 0}
+                        annealing_paramters = {'T': 10, 't': args.usa_t}
                         )
                     )
     usas = torch.nn.ModuleList(usas).to("cuda:0")
     if args.usa_load is not None:
         usas.load_state_dict(torch.load(args.usa_load))
+    print("t value", usas[0].t, flush=True)
     
     span_criterion = torch.nn.BCELoss() # Define your loss function (e.g., torch.nn.CrossEntropyLoss())
     optimizer = torch.optim.Adam(usas.parameters()) # Define your optimizer (e.g., torch.optim.Adam(model.parameters()))
-
-    train_usa(model, usas, train_data_loader, test_data_loader, args.usa_target_mode, optimizer, span_criterion, args.max_train_itr)
+    print(model)
+    train_usa(model, usas, train_data_loader, test_data_loader, args.usa_target_mode, optimizer, loss_function, args.max_train_itr, args.num_queries, args.accumulate_grad, args.recall_weight, args)
     torch.save(usas.cpu().state_dict(), "./artifacts/usa-" + args.model + "-config-" + 'L_{}-R_{}-I_{}-mode-{}'.format(args.usa_L, args.usa_R, args.usa_int_dim, args.usa_target_mode))
 
 if __name__ == '__main__':
